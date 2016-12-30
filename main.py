7cffe984595b69627c026cba483e03dff4a331ee
lenny <liangnian1155@gmail.com>
2016-12-30 10:13:40 +0800

    init

---
+++ main.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import ocr
+from configparser import ConfigParser
+
+def login(username, password):
+	pass
+
+
+
+def main():
+	pass
+
+
+if __name__ == '__main__':
+	main()
---
+++ ocr/track.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+from configparser import ConfigParser
+
+def main():
+	pass
+
+
+
+if __name__ == '__main__':
+	print('*'*20 + "track" + '*'*20)
