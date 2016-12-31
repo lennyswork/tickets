2aacdddb698b0229c4ebc937f7182a1b5ac90563
lenny <liangnian1155@gmail.com>
2016-12-31 14:30:12 +0800

    image

--- config.py
+++ config.py
@@ -1,7 +1,25 @@
+#!/usr/bin/env python3
+
+# 读取配置文件
 from configparser import ConfigParser
 
 class Config(object):
-	"""read the config"""
-	def __init__(self, arg):
-		super(Config, self).__init__()
-		self.arg = arg
+	"""加载配置文件"""
+	def __init__(self):
+		self.config = self.read('config.ini')
+
+
+	def read(self, filenames):
+		cfg = ConfigParser()
+		cfg.read(filenames)
+		return cfg
+
+
+
+if __name__ == '__main__':
+	config = Config().config
+	print(config.get('url','simple_sample_url'))
+	
+
+
+
--- ocr/track.py
+++ ocr/track.py
@@ -1,10 +1,26 @@
 #!/usr/bin/env python3
 from configparser import ConfigParser
+from PIL import Image
 
 def main():
-	pass
+    im = Image.open('../picture/file.bmp')
+    imgry = im.convert('L')
+    imgry.show()
+    threshold = 140
+    table = []
+    for i in range(256):
+        if i < threshold:
+            table.append(0)
+        else:
+            table.append(1)
+    out = imgry.point(table, '1')
+    out.show()
+
+
+
 
 
 
 if __name__ == '__main__':
-	print('*'*20 + "track" + '*'*20)
+    print('*'*20 + "track" + '*'*20)
+    main()
